56bd48ee60b2ddaa2e22b6466231f81d0e7314de
hrnicek <hrncir39@gmail.com>
2025-11-25 14:09:08 +0100

    wip

--- resources/views/components/card-event.blade.php
+++ resources/views/components/card-event.blade.php
@@ -9,12 +9,12 @@
         </span>
       @endif
 
-      @if($event->event_date)
+      @if($event->datetime_from)
         <span class="text-sm text-zinc-500 flex items-center gap-1">
           <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
           </svg>
-          {{ $event->formatted_event_date }}
+          {{ $event->datetime_from->format('j.n Y') }}
         </span>
       @endif
     </div>
@@ -42,4 +42,3 @@
     </div>
   </div>
 </div>
-
