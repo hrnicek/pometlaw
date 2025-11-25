d35729d7ac8f96e3c36e03b6634f6d322ed56a25
hrnicek <hrncir39@gmail.com>
2025-11-25 12:04:21 +0100

    update

--- resources/views/components/testimonials-section.blade.php
+++ resources/views/components/testimonials-section.blade.php
@@ -4,9 +4,9 @@
       <div class="w-full md:w-1/2 px-4 mb-12 md:mb-0">
         <div class="max-w-lg mx-auto md:mx-0 overflow-hidden">
           <div class="flex -mx-4 transition-transform duration-500" :style="'transform: translateX(-' + (activeSlide - 1) * 100 + '%)'">
-            <img class="block flex-shrink-0 w-full px-4 rounded-lg border border-gray-20" src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=400&h=600&fit=crop&crop=center" alt="Klient"/>
-            <img class="block flex-shrink-0 w-full px-4 rounded-lg border border-gray-20" src="https://images.unsplash.com/photo-1494790108755-2616b612b786?w=400&h=600&fit=crop&crop=center" alt="Klientka"/>
-            <img class="block flex-shrink-0 w-full px-4 rounded-lg border border-gray-20" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=600&fit=crop&crop=center" alt="Klient"/>
+            <img class="block flex-shrink-0 w-full px-4 rounded-lg border border-gray-20" src="/img/petrpernica.jpeg" alt="Klient"/>
+            <img class="block flex-shrink-0 w-full px-4 rounded-lg border border-gray-20" src="/img/petrpernica.jpeg" alt="Klientka"/>
+            <img class="block flex-shrink-0 w-full px-4 rounded-lg border border-gray-20" src="/img/petrpernica.jpeg" alt="Klient"/>
           </div>
         </div>
       </div>
@@ -15,18 +15,18 @@
           <div class="flex -mx-4 transition-transform duration-500" :style="'transform: translateX(-' + (activeSlide - 1) * 100 + '%)'">
             <div class="flex-shrink-0 px-4 w-full">
               <h4 class="text-3xl lg:text-4xl font-medium mb-10 text-delft-blue">"Pometlaw transformoval způsob, jakým pracujeme s veřejnými zakázkami. Efektivní, zelené technologie, vynikající služby!"</h4>
-              <span class="block text-xl font-medium text-delft-blue">Jan Novák</span>
-              <span class="block mb-12 lg:mb-32 text-lg text-gray-50">Manažer veřejného sektoru</span>
+              <span class="block text-xl font-medium text-delft-blue">Petr Pernica</span>
+              <span class="block mb-12 lg:mb-32 text-lg text-gray-50">Advokát</span>
             </div>
             <div class="flex-shrink-0 px-4 w-full">
               <h4 class="text-3xl lg:text-4xl font-medium mb-10 text-delft-blue">"Efektivní, zelené technologie, vynikající služby"</h4>
-              <span class="block text-xl font-medium text-delft-blue">Marie Svobodová</span>
-              <span class="block mb-12 lg:mb-32 text-lg text-gray-50">CEO, Zelené řešení s.r.o.</span>
+              <span class="block text-xl font-medium text-delft-blue">Petr Pernica</span>
+              <span class="block mb-12 lg:mb-32 text-lg text-gray-50">Advokát</span>
             </div>
             <div class="flex-shrink-0 px-4 w-full">
               <h4 class="text-3xl lg:text-4xl font-medium mb-10 text-delft-blue">"Pometlaw transformoval způsob, jakým pracujeme s veřejnými zakázkami, efektivní, zelené technologie, vynikající služby."</h4>
-              <span class="block text-xl font-medium text-delft-blue">Petr Dvořák</span>
-              <span class="block mb-12 lg:mb-32 text-lg text-gray-50">Vývojář</span>
+              <span class="block text-xl font-medium text-delft-blue">Petr Pernica</span>
+              <span class="block mb-12 lg:mb-32 text-lg text-gray-50">Advokát</span>
             </div>
           </div>
           <div>
